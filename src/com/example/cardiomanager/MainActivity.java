c59d45c533913dda1e42c14fa4440a7e2cf5690b
Varkh <den3@ukr.net>
2013-05-22 15:41:03 +0300

    little changes

--- src/com/example/cardiomanager/MainActivity.java
+++ src/com/example/cardiomanager/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -30,6 +31,8 @@ public class MainActivity extends Activity {
         audioFilter = new AudioFilter(MainActivity.this);
         microphoneRecoder = new MicrophoneRecoder(audioFilter);
         mProgress = (ProgressBar) findViewById(R.id.progressBar);
+        
+        
     }
 
     @Override
@@ -45,6 +48,11 @@ public class MainActivity extends Activity {
     	text = (TextView) findViewById(R.id.editView);
     	text.setText("Started");
     	microphoneRecoder.startRecording();
+    	
+    	Button btn = (Button)findViewById(R.id.btnStart);
+    	btn.setEnabled(false);
+    	btn = (Button)findViewById(R.id.btnStop);
+    	btn.setEnabled(true);
     }
     
     public void onBtnStopClick(View view) {
@@ -53,15 +61,23 @@ public class MainActivity extends Activity {
     	text.setText("Stoped");
     	microphoneRecoder.stopRecording();
     	setProgresBar(0);
+    	
+    	Button btn = (Button)findViewById(R.id.btnStop);
+    	btn.setEnabled(false);
+    	btn = (Button)findViewById(R.id.btnStart);
+    	btn.setEnabled(true);
     }
     
-    public void setProgresBar(int value)
+    public synchronized void setProgresBar(int value)
     {
     	mProgressStatus = (int)(value);///2.55);
     	// Update the progress bar
         mHandler.post(new Runnable() {
             public void run() {
                 mProgress.setProgress(mProgressStatus);
+                
+                TextView ed = (TextView) findViewById(R.id.editView);
+                ed.setText(Integer.toString(mProgressStatus));
             }
         });
     }
