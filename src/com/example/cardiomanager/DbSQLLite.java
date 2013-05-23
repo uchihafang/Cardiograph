ca754b16e1e460ebd9df2e88059cd07fca057b46
Varkh <den3@ukr.net>
2013-05-23 18:38:55 +0300

    Add database again

---
+++ src/com/example/cardiomanager/DbSQLLite.java
@@ -0,0 +1,35 @@
+package com.example.cardiomanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbSQLLite extends SQLiteOpenHelper {
+
+	//DataBasePropetis
+	private static final int DB_VERSION = 1;  
+	private static final String DB_NAME = "CardioBase";
+
+	public static final String TABLE_NAME = "users";
+	public static final String USERNAME = "username";
+	public static final String DATEBRTH = "datebrth";
+	public static final String HEIGHT = "height";
+	public static final String WEIGHT = "weight";
+	public static final String DASEASES = "DESEASES";
+	private static final String CREATE_TABLE = "create table " + TABLE_NAME + " ( _id integer primary key autoincrement, "
+	      + USERNAME + " TEXT, " + DATEBRTH + " TEXT, " + HEIGHT + " INTEGER, " + WEIGHT + " INTEGER, " + DASEASES + " TEXT)";
+
+	public DbSQLLite(Context context) {
+	    super(context, DB_NAME, null, DB_VERSION);
+	  }
+
+	  @Override
+	  public void onCreate(SQLiteDatabase sqLiteDatabase) {
+	    sqLiteDatabase.execSQL(CREATE_TABLE);
+	  }
+
+	  @Override
+	  public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+	  }
+
+}
