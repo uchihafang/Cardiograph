c98a0a2aa4747370a866de43e5ae18d9654fc2ef
Varkh <den3@ukr.net>
2013-05-23 17:27:28 +0300

    Add Check audio formats, edit personal activity

--- src/com/example/cardiomanager/AudioFilter.java
+++ src/com/example/cardiomanager/AudioFilter.java
@@ -1,5 +1,7 @@
 package com.example.cardiomanager;
 
+import android.widget.Toast;
+
 public class AudioFilter {
 	
 	byte Data[];
@@ -17,6 +19,34 @@ public class AudioFilter {
 	public synchronized byte[] getBuffer() {
 		// TODO
 		return Data;
+	}
+	
+	public synchronized void showMassage(int massageN) {
+		
+		Toast toast = Toast.makeText(activity.getApplicationContext(), 
+				   "Пора покормить кота!", Toast.LENGTH_SHORT);
+		switch (massageN) {
+		case 1:
+			toast.setText("Initialization Start");
+			break;
+		case 2:
+			toast.setText("Initialization Finish");
+			break;
+		case 3:
+			toast.setText("Object created");
+			break;
+		case 4:
+			toast.setText("Thread started");
+			break;
+		}
+		toast.show();
+	}
+	
+	public synchronized void showFormat(int r, int c, int m ) {
+		
+		Toast toast = Toast.makeText(activity.getApplicationContext(), 
+				   Integer.toString(r)+" "+Integer.toString(c)+" "+Integer.toString(m), Toast.LENGTH_SHORT);
+		toast.show();
 	}
 
 }
--- src/com/example/cardiomanager/MicrophoneRecoder.java
+++ src/com/example/cardiomanager/MicrophoneRecoder.java
@@ -18,24 +18,46 @@ public class MicrophoneRecoder
 	private Thread recordingThread = null;
 	private boolean isRecording = false;
 	private AudioFilter audioFilter;
-	
+	private int bufferSize;
 	
 	public MicrophoneRecoder(AudioFilter aF)
 	{
-		int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
+		//aF.showMassage(1);
+		bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
 	            RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
 		audioFilter = aF;
+		aF.showMassage(2);
+		
+		int[] rates = {8000, 11025, 22050, 44100, 48000, 96000 };
+		int[] chans = {AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO};
+		int[] encs  = {AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT};
+
+		for(int enc : encs)
+		{
+		    for(int ch : chans)
+		    {
+		        for(int rate : rates)
+		        {
+		            int t = AudioRecord.getMinBufferSize(rate, ch, enc);
+		            
+		            if((t != AudioRecord.ERROR) && (t != AudioRecord.ERROR_BAD_VALUE))
+		            {
+		                //aF.showFormat(rate, ch, enc);
+		            }
+		        }
+		    }
+		}
 	}
 	    
 	int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
 	int BytesPerElement = 2; // 2 bytes in 16bit format
 	
 	public void startRecording() {
-
 	    recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
 	            RECORDER_SAMPLERATE, RECORDER_CHANNELS,
-	            RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
-	    
+	            RECORDER_AUDIO_ENCODING, bufferSize*10);
+	            //BufferElements2Rec * BytesPerElement);
+	    //audioFilter.showMassage(3);
 	    recorder.startRecording();
 	    isRecording = true;
 	    recordingThread = new Thread(new Runnable() {
@@ -61,7 +83,7 @@ public class MicrophoneRecoder
 
 	private void writeAudioDataToFile() {
 	    // Write the output audio in byte
-
+		//audioFilter.showMassage(4);
 	    //String filePath = "/sdcard/voice8K16bitmono.pcm";
 	    short sData[] = new short[BufferElements2Rec];
 
